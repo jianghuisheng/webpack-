d2b4cf8dd27a1ad22c3d6e33e45ede5effa6177b
jianghuisheng <1697965700@qq.com>
2021-01-22 17:36:09 +0800

    13.js兼容性处理eslint

--- 13.js兼容性处理eslint/src/js/index.js
+++ 13.js兼容性处理eslint/src/js/index.js
@@ -1,6 +1,21 @@
-function add(x, y) {
-  return x + y;
-}
-
+// const add = function add(x, y) {
+//   return x + y;
+// };
 // eslint-disable-next-line
-console.log(add(2, 5));
+// console.log(add(2, 5));
+// 直接引入
+import '@babel/polyfill'; // import 'core-js/stable'
+// import 'regenerator-runtime'
+
+const add = function add(x, y) {
+  return x + y;
+};
+
+add(2, 5);
+const promise = new Promise((resolve) => {
+  setTimeout(() => {
+    console.log('定时器执行完了~');
+    resolve();
+  }, 1000);
+});
+console.log(promise);
--- 13.js兼容性处理eslint/webpack.config.js
+++ 13.js兼容性处理eslint/webpack.config.js
@@ -9,24 +9,56 @@ module.exports = {
   },
   module: {
     rules: [
+      /**
+       * js兼容性处理：babel-loader @babel/core @babel/preset-env
+       * 1. 基本js兼容性处理 --> @babel/preset-env
+       *    问题：只能转换基本语法，如promise不能转换
+       * 2. 全部兼容性处理 --> @babel/polyfill
+       *    问题：我只要解决部分兼容性问题，但是将所有兼容性代码全部引入，体积太大了
+       * 3. 需要做兼容性处理的就做：按需加载 --> core-js
+       *
+       */
       {
-        /**
-         * 语法检查：eslint-loader eslint
-         * 注意：只检查自己写的源代码，第三方的库是不用检查的
-         * 设置检查规则：
-         *     package.json中的eslintConfig中设置
-         *   "eslintConfig": {
-         *     "extends": "airbnb-base"
-         *    }
-         *    airbnb -->eslint eslint-config-airbnb-base eslint-plugin-import
-         */
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'eslint-loader',
-        options: {
-          // 自动修复eslint错误
-          fix: true,
-        },
+        use: [
+          {
+            loader: 'eslint-loader',
+            options: {
+              fix: true,
+            },
+          },
+          // es6 --> es5
+          {
+            loader: 'babel-loader',
+            options: {
+              // 预设：指示babel做怎么样的兼容性处理
+              presets: ['@babel/preset-env'],
+
+              // presets: [
+              //   [
+              //     '@babel/preset-env',
+              //     {
+              //       // 按需加载
+              //       useBuiltIns: 'usage',
+              //       // 指定core-js版本
+              //       corejs: {
+              //         version: 3,
+              //       },
+              //       // 指定兼容性做到哪个版本浏览器
+              //       targets: {
+              //         chrome: '60',
+              //         firefox: '60',
+              //         ie: '9',
+              //         safari: '10',
+              //         edge: '17',
+              //       },
+              //     },
+              //   ],
+              // ],
+            },
+          },
+        ],
       },
     ],
   },
