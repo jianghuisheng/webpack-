28c0fbe134d7160f8a732da49ae707e52c441296
jianghuisheng <1697965700@qq.com>
2021-01-29 16:14:40 +0800

    懒加载和预加载

--- 24.懒加载和预加载/webpack.config.js
+++ 24.懒加载和预加载/webpack.config.js
@@ -2,8 +2,10 @@ const { resolve } = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  // 单入口
   entry: './src/js/index.js',
   output: {
+    // [name]: 取文件名
     filename: 'js/[name].[contenthash:10].js',
     path: resolve(__dirname, 'build'),
   },
@@ -16,6 +18,11 @@ module.exports = {
       },
     }),
   ],
+  /* 
+    可以将node_modules中代码单独打包一个chunk最终输出
+    自动分析多入口chunk中，有没有公共的文件。如果有会打包成一个单独的chunk(块)
+    作用：避免代码臃肿，分割成多个小模块并行加载，提高效率
+  */
   optimization: {
     splitChunks: {
       chunks: 'all',
