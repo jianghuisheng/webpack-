7dbd80725b6006208b40e0d3b87bec5b439d2546
jianghuisheng <1697965700@qq.com>
2021-02-01 14:48:46 +0800

    多进程打包

---
+++ 26.多进程打包/webpack.config.js
@@ -0,0 +1,143 @@
+const { resolve } = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
+
+/**
+ * PWA：渐进式网络开发应用程序（离线可访问）
+ *   workbox --> workbox-webpack-plugin
+ */
+
+// 定义nodejs环境变量：决定使用browserslist的哪个环境
+process.env.NODE_ENV = 'production'
+
+// 复用loader
+const commonCssLoader = [
+  MiniCssExtractPlugin.loader,
+  'css-loader',
+  {
+    loader: 'postcss-loader',
+    options: {
+      postcssOptions: {
+        indent: 'postcss',
+        plugins: [require('autoprefixer')],
+      },
+    },
+  },
+]
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/built.[contenthash:10].js',
+    path: resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // 优先执行
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'eslint-loader',
+            options: {
+              fix: true,
+            },
+          },
+        ],
+      },
+      {
+        // 以下loader只会匹配一个，提升构建速度
+        // 注意：不能有两个配置处理同一种类型文件
+        oneOf: [
+          {
+            test: /\.css$/,
+            use: [...commonCssLoader],
+          },
+          {
+            test: /\.less$/,
+            use: [...commonCssLoader, 'less-loader'],
+          },
+
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [
+              /**
+               *  开启多进程打包
+               *  进程启动大概为600ms，进程通信也有开销（有利有弊）
+               *  只有工作消耗时间比较长才需要
+               */
+              // 'thread-loader',
+              {
+                loader: 'thread-loader',
+                options: {
+                  workers: 2, // 进程2个
+                },
+              },
+              {
+                loader: 'babel-loader',
+                options: {
+                  presets: ['@babel/preset-env'],
+                  // 开启babel缓存
+                  // 第二次构建时，会读取之前的缓存，刷新浏览器可以看到效果(会出现强制缓存bug,需要加版本号)
+                  cacheDirectory: true,
+                },
+              },
+            ],
+          },
+          {
+            test: /\.(jpg|png|gif)$/,
+            loader: 'url-loader',
+            options: {
+              limit: 8 * 1024,
+              name: '[contenthash:10].[ext]',
+              outputPath: 'imgs',
+              esModule: false,
+            },
+          },
+          {
+            test: /\.html$/,
+            loader: 'html-loader',
+          },
+          {
+            exclude: /\.(js|css|less|html|jpg|png|gif)$/,
+            loader: 'file-loader',
+            options: {
+              outputPath: 'media',
+              name: '[hash:10].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/built.[hash:10].css',
+    }),
+    new OptimizeCssAssetsPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minify: {
+        collapseWitespace: true,
+        removeComment: true,
+      },
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      /* 
+        1.帮助 serviceworker 快速启动
+        2.删除旧的 serviceworker
+
+        生成一个 serviceworker 配置文件~
+      */
+      clientsClaim: true,
+      skipWaiting: true,
+    }),
+  ],
+  mode: 'production',
+  devtool: 'source-map',
+}
